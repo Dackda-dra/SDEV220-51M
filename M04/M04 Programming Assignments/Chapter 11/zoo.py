fab4f50ec104dadcbe49c1df6db5dc4bbee19c15
Dackda-dra <99042949+Dackda-dra@users.noreply.github.com>
2023-02-11 00:31:13 -0500

    Delete M04 directory

--- M04/M04 Programming Assignments/Chapter 11/zoo.py
+++
@@ -1,6 +0,0 @@
-# Drashaun Morrow 11.1
-#zoo.py
-# This function will display a string and is to be called to a different program.
-
-def hours():
-    print("Open 9-5 Daily")
--- M04/M04 Programming Assignments/Chapter 11/zoo11.2.py
+++
@@ -1,8 +0,0 @@
-# Drashaun Morrow
-# zoo11.2
-
-# continuation of 11.1 into 11.2
-
-import zoo as menagerie
-
-menagerie.hours()
